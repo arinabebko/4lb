39f392afc3ebcc9c48acfcd8212d7eea500f6229
prodUpal <tim.ism06@gmail.com>
2024-10-15 14:05:30 +0500

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bebko_Autoservice
+{
+    internal class Class1
+    {
+    }
+}
